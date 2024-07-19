eeeaffa57adb233ddcbdf319125573ca823858eb
helgituan <helgih20@ru.is>
2024-07-19 22:25:40 +0000

    Another for images

--- next.config.js
+++ next.config.js
@@ -1,10 +1,11 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === 'production';
+/*const isProd = process.env.NODE_ENV === 'production';*/
 
 const nextConfig = {
   output: 'export',
-  basePath: isProd ? '/public' : '', // Adjust according to your deployment setup
+  /*basePath: isProd ? '/public' : '', // Adjust according to your deployment setup */
+  basePath: 'public', // Adjust according to your deployment setup
   assetPrefix: '/Pages/', // Adjust according to your deployment setup
 };
 
